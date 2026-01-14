61c6fff81a74e11d8b703ba6d0de8229370896d1
merisssas <swithjaya@gmail.com>
2026-01-14 18:09:27 +0800

    Update main.go

--- main.go
+++ main.go
@@ -4,35 +4,75 @@ package main
 import (
 	_ "embed"
 	"flag"
+	"os"
+	"strings"
 
-	"github.com/cloudreve/Cloudreve/v4/cmd"
-	"github.com/cloudreve/Cloudreve/v4/pkg/util"
+	"github.com/merisssas/Cloudreve/v4/cmd"
+	"github.com/merisssas/Cloudreve/v4/pkg/util"
 )
 
 var (
-	isEject    bool
 	confPath   string
 	scriptName string
 )
 
-func init() {
-	flag.BoolVar(&util.UseWorkingDir, "use-working-dir", false, "Use working directory, instead of executable directory")
-	flag.StringVar(&confPath, "c", util.RelativePath("conf.ini"), "Path to the config file.")
-	flag.StringVar(&scriptName, "database-script", "", "Name of database util script.")
-	//flag.Parse()
+// envBool membaca env var boolean yang “manusiawi”.
+// Accepted true: 1, true, yes, y, on (case-insensitive)
+func envBool(key string, def bool) bool {
+	v := strings.TrimSpace(os.Getenv(key))
+	if v == "" {
+		return def
+	}
+	switch strings.ToLower(v) {
+	case "1", "true", "yes", "y", "on":
+		return true
+	case "0", "false", "no", "n", "off":
+		return false
+	default:
+		return def
+	}
+}
 
-	//staticFS = bootstrap.NewFS(staticZip)
-	//bootstrap.Init(confPath, staticFS)
+func init() {
+	// Default: pakai executable directory (sesuai util.UseWorkingDir = false)
+	// Bisa di-override dengan env (berguna untuk Docker/CI):
+	//   CLOUDREVE_USE_WORKING_DIR=1
+	defaultUseWD := envBool("CLOUDREVE_USE_WORKING_DIR", false)
+	util.UseWorkingDir = defaultUseWD
+
+	// Default config Cloudreve v4 adalah data/conf.ini. 2
+	defaultConf := os.Getenv("CLOUDREVE_CONFIG")
+	if strings.TrimSpace(defaultConf) == "" {
+		defaultConf = util.DataPath("conf.ini")
+	}
+
+	flag.BoolVar(
+		&util.UseWorkingDir,
+		"use-working-dir",
+		defaultUseWD,
+		"Use working directory instead of executable directory (env: CLOUDREVE_USE_WORKING_DIR)",
+	)
+
+	flag.StringVar(
+		&confPath,
+		"c",
+		defaultConf,
+		"Path to the config file (default: data/conf.ini; env: CLOUDREVE_CONFIG)",
+	)
+
+	flag.StringVar(
+		&scriptName,
+		"database-script",
+		"",
+		"Name of database util script (reserved; prefer cmd subcommands if available).",
+	)
+
+	// IMPORTANT:
+	// Jangan flag.Parse() di sini kalau cmd.Execute() memakai Cobra/pflag dan menggabungkan Go flags.
+	// Kalau kamu parse di sini, bisa bentrok dengan flags milik Cobra.
 }
 
 func main() {
+	// Entry point utama Cloudreve CLI (server/migrate/eject/etc).
 	cmd.Execute()
-	return
-	// 关闭数据库连接
-
-	//if scriptName != "" {
-	//	// 开始运行助手数据库脚本
-	//	bootstrap.RunScript(scriptName)
-	//	return
-	//}
 }
